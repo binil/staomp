c4553f120e6f8c5ef1e8a8ab1bd760f1752d5705
Binil Thomas <binil.thomas@gmail.com>
2010-12-20 00:24:21 -0800

    Added ScalaTest dependency

--- project/build/staompProject.scala
+++ project/build/staompProject.scala
@@ -2,4 +2,5 @@ import sbt._
 
 class staompProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
   val sc = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" withSources()
+  val st = "org.scalatest" % "scalatest" % "1.2" withSources()
 }
